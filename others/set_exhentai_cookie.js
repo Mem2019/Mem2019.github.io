79d20be5d98a9894921ac994388b2326478f5329
2019 <434791602@qq.com>
2018-11-11 22:24:24 +0000

    Update set_exhentai_cookie.js

--- others/set_exhentai_cookie.js
+++ others/set_exhentai_cookie.js
@@ -1,1 +1,6 @@
-document.cookie="__cfduid=dd7971c4b57b739177bcad8d94af657001511034815; ipb_member_id=1698216; ipb_pass_hash=8172fb821c0a7a4b4b4f9f59b11dbcbf; ipb_session_id=24f59ce0e1d9da42ec03317a4b823a9f; igneous=4368b1c17; lv=1511036296-1511036296"
+document.cookie="__cfduid=dd7971c4b57b739177bcad8d94af657001511034815;"
+document.cookie+="ipb_member_id=1698216;"
+document.cookie+="ipb_pass_hash=8172fb821c0a7a4b4b4f9f59b11dbcbf;"
+document.cookie+="ipb_session_id=24f59ce0e1d9da42ec03317a4b823a9f;" 
+document.cookie+="igneous=4368b1c17;"
+document.cookie+="lv=1511036296-1511036296"
