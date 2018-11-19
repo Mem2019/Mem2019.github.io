d73a2f321f540ea463e44d279bbf9659d7e2716f
2019 <434791602@qq.com>
2018-11-19 23:14:43 +0000

    test

--- others/set_exhentai_cookie.js
+++ others/set_exhentai_cookie.js
@@ -1,3 +1,4 @@
+document.cookie=""
 document.cookie="__cfduid=dd7971c4b57b739177bcad8d94af657001511034815;";
 document.cookie+="ipb_member_id=1698216;";
 document.cookie+="ipb_pass_hash=8172fb821c0a7a4b4b4f9f59b11dbcbf;";
