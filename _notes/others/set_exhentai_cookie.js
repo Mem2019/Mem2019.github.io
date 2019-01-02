317e882dd7db4924b390ddb92412b2fe787979b3
2019 <434791602@qq.com>
2019-01-02 22:08:29 +0800

    Jekyll blog

--- _notes/others/set_exhentai_cookie.js
+++
@@ -1,7 +0,0 @@
-document.cookie=""
-document.cookie="__cfduid=dd7971c4b57b739177bcad8d94af657001511034815;";
-document.cookie+="ipb_member_id=1698216;";
-document.cookie+="ipb_pass_hash=8172fb821c0a7a4b4b4f9f59b11dbcbf;";
-document.cookie+="ipb_session_id=24f59ce0e1d9da42ec03317a4b823a9f;" ;
-document.cookie+="igneous=4368b1c17;";
-document.cookie+="lv=1511036296-1511036296";
